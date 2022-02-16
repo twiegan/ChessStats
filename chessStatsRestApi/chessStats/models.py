132c9e6266a5ed9011155a8a3607e5876c01cd15
Kichul Kang <kckang1103@gmail.com>
2022-02-16 11:46:37 -0500

    initial Django setup

---
+++ chessStatsRestApi/chessStats/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+class Player(models.Model):
+  player_id = models.CharField(max_length=15, blank=False, default='')
+  title = models.CharField(max_length=2, blank=False, default='')
