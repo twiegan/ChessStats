0431622818a97bacaf0e31770aaa9628056039f8
twiegan <twiegan@purdue.edu>
2022-02-28 22:29:31 -0500

    Jesus christ

--- chessStatsRestApi/settings.py
+++ chessStatsRestApi/settings.py
@@ -16,7 +16,6 @@ import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-print(f'BASE_DIR: {BASE_DIR}')
 
 # Initialize environment variables
 
@@ -29,6 +28,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, 'chessStatsRestApi/.env'))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
