ae68f1deb9e1082e6fac32a416f5be8ae303eca1
Kichul Kang <kckang1103@gmail.com>
2022-03-02 21:38:34 -0500

    added a new model

--- chessStats/serializers.py
+++ chessStats/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
 from chessStats.models import Player
 
+from chessStats.models import Test
+
 
 class PlayerSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Player
         fields = ('player_id', 'title')
+
+class TestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Test
+        fields = ('Shravan', )
