b8560d1b18913cdcb509e3948ba038975c35fedc
Kichul Kang <kckang1103@gmail.com>
2022-03-21 17:58:24 -0400

    tested material ui

--- chess-app/src/app/app.module.ts
+++ chess-app/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatSliderModule } from '@angular/material/slider';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
