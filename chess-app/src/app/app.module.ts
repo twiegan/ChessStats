7649dd2826c4c3a97aff9fb7bb70a1e3ca5a4cd8
Tiger Lee <lee3189@purdue.edu>
2022-03-21 17:55:16 -0400

    routing to pages

--- chess-app/src/app/app.module.ts
+++ chess-app/src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { appRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home';
+import { EntryComponent } from './entry';
+import { FollowedComponent } from './followed';
+import { LoginComponent } from './login';
+import { SearchComponent } from './search';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EntryComponent,
+    FollowedComponent,
+    LoginComponent,
+    SearchComponent
+
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    appRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ chess-app/src/app/entry/entry.component.ts
@@ -0,0 +1,4 @@
+import { Component } from '@angular/core';
+
+@Component({ templateUrl: 'entry.component.html' })
+export class EntryComponent {}
