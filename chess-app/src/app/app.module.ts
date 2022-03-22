5f3ced104df29c4f250a03b513d944aeb51e8a2e
Kichul Kang <kckang1103@gmail.com>
2022-03-22 00:22:14 -0400

    frontend and backend completed for player entry

--- chess-app/src/app/app.module.ts
+++ chess-app/src/app/app.module.ts
@@ -11,14 +11,24 @@ import { LoginComponent } from './login';
 import { SearchComponent } from './search';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
+import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSliderModule } from '@angular/material/slider';
-import {MatInputModule} from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ReactiveFormsModule } from '@angular/forms';
 
 const mat = [
+  MatButtonModule,
   MatFormFieldModule,
+  MatIconModule,
   MatInputModule,
+  MatSnackBarModule,
+  MatToolbarModule,
+  ReactiveFormsModule,
 ];
 
 @NgModule({
@@ -35,6 +45,7 @@ const mat = [
     BrowserModule,
     BrowserAnimationsModule,
     appRoutingModule,
+    HttpClientModule,
     ...mat,
   ],
   exports: [
