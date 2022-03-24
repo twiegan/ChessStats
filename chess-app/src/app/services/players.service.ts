51b04b0e4836d4411f1d1d1ef3f9c6e1cf1c5018
Kichul Kang <kckang1103@gmail.com>
2022-03-23 21:47:57 -0400

    updated player model to allow blank title

--- chess-app/src/app/services/players.service.ts
+++ chess-app/src/app/services/players.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { Player } from './player'
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +11,11 @@ export class PlayersService {
   url = "https://infinite-river-70119.herokuapp.com/players/"
   constructor(private http: HttpClient) { }
 
-  getPlayers() {
-    return this.http.get(this.url)
+  public getPlayers(): Observable<Player[]> {
+    return this.http.get<Player[]>(this.url);
   }
 
-  addPlayer(data: any) {
-    return this.http.post(this.url, data)
+  public addPlayer(data: any) {
+    return this.http.post(this.url, data);
   }
 }
