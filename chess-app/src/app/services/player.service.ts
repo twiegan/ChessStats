58056c9f4f3b00e8540e4b29db1262c25231cab7
twiegan <twiegan@purdue.edu>
2022-05-01 17:04:46 -0400

    playerStats-search combo page complete

--- chess-app/src/app/services/player.service.ts
+++ chess-app/src/app/services/player.service.ts
@@ -3,12 +3,14 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { Player } from './player'
+import { MatchWithName } from './matchWithName';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlayerService {
   url = "https://infinite-river-70119.herokuapp.com/players/"
+  matchUrl = "http://localhost:5000/search/player/"
   ret: any
   constructor(private http: HttpClient) { }
 
@@ -18,6 +20,10 @@ export class PlayerService {
 
   public getPlayer(playerId: any): Observable<Player> {
     return this.http.get<Player>(this.url + playerId)
+  }
+  
+  public getMatches(playerId: any): Observable<MatchWithName> {
+    return this.http.get<MatchWithName>(this.matchUrl + playerId);
   }
 
   public addPlayer(data: any) {
